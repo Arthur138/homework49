986a28a30deb140468fad823208357ff50939504
Arthur <arturkuliev2@gmail.com>
2022-11-18 17:42:48 +0600

    forms were fixed

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,8 +1,9 @@
 from django import forms
 from webapp.models import Status, Type
+from django.forms import widgets
 
 class DoingForm(forms.Form):
-    summary = forms.CharField(max_length=50, required=True, label='Summary')
-    description = forms.CharField(max_length = 150, required=False, label='Description')
+    summary = forms.CharField(max_length=200, required=True, label='Summary')
+    description = forms.CharField(max_length = 2000, required=False, label='Description', widget=widgets.Textarea)
     status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Status')
     type = forms.ModelChoiceField(queryset=Type.objects.all(), label='Type')
