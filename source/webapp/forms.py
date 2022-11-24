6980fbe5e1afa2a01f653f00501803f941b1169f
Arthur <arturkuliev2@gmail.com>
2022-11-25 01:43:48 +0600

    model forms added

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,9 +1,21 @@
 from django import forms
-from webapp.models import Status, Type
-from django.forms import widgets
+from webapp.models import Status, Type , Doings
+from django.forms import widgets , ValidationError
 
-class DoingForm(forms.Form):
-    summary = forms.CharField(max_length=200, required=True, label='Summary')
-    description = forms.CharField(max_length = 2000, required=False, label='Description', widget=widgets.Textarea)
-    status = forms.ModelChoiceField(queryset=Status.objects.all(), label='Status')
-    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label='Type' , widget=widgets.CheckboxSelectMultiple)
+class DoingForm(forms.ModelForm):
+    class Meta:
+        model = Doings 
+        fields = ['summary', 'description', 'status', 'type']
+        widgets = {'description':widgets.Textarea, 'type':widgets.CheckboxSelectMultiple}
+
+    def clean_summary(self):
+        summary = self.cleaned_data['summary']
+        if len(summary) < 3:
+            self.add_error('summary', ValidationError('*Введите больше 3 символов'))
+        return summary
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['summary'] == cleaned_data['description']:
+            raise ValidationError('Описание не должно совпадать с названием')
+        return cleaned_data
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render , redirect , get_object_or_404
+from django.shortcuts import render , redirect , get_object_or_404 
 from webapp.models import Doings , Status , Type
-# from django.http import HttpResponseRedirect , HttpResponseNotFound
 from webapp.forms import DoingForm
-from django.views.generic import View , TemplateView
+from django.urls import reverse
+from django.views.generic import View , TemplateView ,FormView
 # Create your views here.
 
 
@@ -22,44 +22,51 @@ class DoingView(TemplateView):
        context['doing'] = get_object_or_404(Doings, pk=kwargs['pk'])
        return context
 
-class DoingCreate(TemplateView):
+class DoingCreate(FormView):
     template_name = "doings_create.html"
-    def get(self,request, *args, **kwargs):
-        form = DoingForm()
-        return render(request, "doings_create.html", {'form': form})
-    def post(self,request, *args, **kwargs):
-        form = DoingForm(data=request.POST)
-        if form.is_valid():
-            type = form.cleaned_data.pop('type')
-            new_doing = Doings.objects.create(**form.cleaned_data)
-            new_doing.type.set(type)
-            return redirect('doing_view', pk=new_doing.pk)
-        else:
-            return render(request, "doings_create.html", {'form': form})
+    form_class = DoingForm
 
-class DoingUpdateView(TemplateView):
-    template_name = "update.html"
-    def get(self,request, *args, **kwargs):
-        doing = get_object_or_404(Doings, pk=kwargs['pk'])
-        form = DoingForm(initial={
-            'summary':doing.summary,
-            'description':doing.description,
-            'status':doing.status,
-            'type':doing.type.all()
-        })
-        return render(request, "update.html", {'form': form})
-    def post(self, request, *args, **kwargs):
-        doing = get_object_or_404(Doings, pk=kwargs['pk'])
-        form = DoingForm(data=request.POST)
-        if form.is_valid():
-            doing.summary = form.cleaned_data.get('summary')
-            doing.description = form.cleaned_data.get('description')
-            doing.status = form.cleaned_data.get('status')
-            doing.save()
-            doing.type.set(form.cleaned_data['type'])
-            return redirect('doing_view', pk=doing.pk)
-        else:
-            return render(request, "doings_create.html", {'form': form})
+    def form_valid(self, form):
+       self.doing = form.save()
+       return super().form_valid(form)
+
+    def get_success_url(self):
+       return reverse('doing_view', kwargs={'pk': self.doing.pk})
+
+class DoingUpdateView(FormView):
+    template_name = 'update.html'
+    form_class = DoingForm
+
+    def dispatch(self, request, *args, **kwargs):
+       self.doing = self.get_object()
+       return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+       context = super().get_context_data(**kwargs)
+       context['doing'] = self.doing
+       return context
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.doing
+        return kwargs
+
+    def form_valid(self, form):
+       type = form.cleaned_data.pop('type')
+       for key, value in form.cleaned_data.items():
+           if value is not None:
+               setattr(self.doing, key, value)
+       self.doing.save()
+       self.doing.type.set(type)
+       return super().form_valid(form)
+
+    def get_success_url(self):
+       return reverse('doing_view', kwargs={'pk': self.doing.pk})
+
+    def get_object(self):
+       pk = self.kwargs.get('pk')
+       return get_object_or_404(Doings, pk=pk)
+
 
 class DoingDeleteView(TemplateView):
     def get(self,request, *args, **kwargs):
